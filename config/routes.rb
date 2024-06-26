4ee0854c9f33198b76b3ee49af44aec4b680b973
Ramone Robertson <ramonerobertson10@gmail.com>
2024-06-26 13:16:03 +0900

    Finished routes, controller and views for index and show actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "flats#index"
+
+  get "flats/:id", to: "flats#show", as: "flat"
+
+
 end
